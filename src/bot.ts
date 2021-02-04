00ef39adc6b35667b03b51de58a3bc1c8e968679
DTheLegend <wampamba.daudi@gmail.com>
2021-02-04 12:20:52 +0000

    Allow Underscores to be Used in Messages
    Done by changing the \w in the regex name to an A-Z

--- src/bot.ts
+++ src/bot.ts
@@ -8,7 +8,7 @@ const banlist = ["Memes", "Uncategorized",".git", "Other", "Personification"];
 async function search(message : string) : Promise<Discord.MessageAttachment> {
     const images_fs : fs.Dirent[] = fs.readdirSync('./images', { "withFileTypes":true });
     for(const folder of images_fs.filter(x => x.isDirectory() && !banlist.includes(x.name))) {
-        const language_regex = new RegExp(`(?<=[^\\w'+]|^)${folder.name.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}(?=[^\\w+]|$)`, 'gi');
+        const language_regex = new RegExp(`(?<=[^A-Z'+]|^)${folder.name.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}(?=[^A-Z+]|$)`, 'gi');
         if(language_regex.test(message)) {
             const language_fs : fs.Dirent[] = fs.readdirSync(`./images/${folder.name}`, { "withFileTypes":true }).filter(x => x.isFile());
             const file = `./images/${folder.name}/${language_fs[Math.floor(Math.random() * language_fs.length)].name}`
